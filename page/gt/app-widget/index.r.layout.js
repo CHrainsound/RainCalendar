c97c73a949ff47f3cfb5cbb697e1520c04ff7fae
SoRC <3014386984@qq.com>
2026-06-25 00:33:50 +0800

    新增快捷卡片

--- page/gt/app-widget/index.r.layout.js
+++ page/gt/app-widget/index.r.layout.js
@@ -1,15 +1,48 @@
+import ui from "@zos/ui";
 import { getDeviceInfo } from "@zos/device";
 import { px } from "@zos/utils";
-import { WIDGET_HEIGHT, TITLE_STYLE, TODAY_STYLE, NEXT_HOLIDAY_STYLE } from "./shared-styles.js";
 
-const { width: DEVICE_WIDTH } = getDeviceInfo();
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = getDeviceInfo();
 
-export { WIDGET_HEIGHT, TITLE_STYLE, TODAY_STYLE, NEXT_HOLIDAY_STYLE };
+export const WIDGET_HEIGHT = 180;
+
+export const TITLE_STYLE = {
+  x: px(60),
+  y: px(10),
+  w: DEVICE_WIDTH - px(80),
+  h: px(32),
+  color: 0x888888,
+  text_size: px(26),
+  align_h: ui.align.LEFT,
+  align_v: ui.align.CENTER_V
+};
+
+export const TODAY_STYLE = {
+  x: px(60),
+  y: px(46),
+  w: DEVICE_WIDTH - px(80),
+  h: px(52),
+  color: 0x00bfff,
+  text_size: px(38),
+  align_h: ui.align.LEFT,
+  align_v: ui.align.CENTER_V
+};
 
 export const LINE_STYLE = {
   x: px(50),
   y: px(104),
   w: DEVICE_WIDTH - px(100),
   h: 5,
-  color: 0x444444
+  color: 0xf8f8ff
+};
+
+export const NEXT_HOLIDAY_STYLE = {
+  x: px(60),
+  y: px(114),
+  w: DEVICE_WIDTH - px(80),
+  h: px(52),
+  color: 0xffffff,
+  text_size: px(34),
+  align_h: ui.align.LEFT,
+  align_v: ui.align.CENTER_V
 };
